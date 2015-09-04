e9b943f3d8614e2e1259ba708d25034eb88d476b
coe <coe.app@gmail.com>
2015-09-04 17:49:57 +0900

    Initial Commit

---
+++ WIKCamViewController/WIKCamViewController.h
@@ -0,0 +1,13 @@
+//
+//  WIKCamViewController.h
+//  WIKCamViewController
+//
+//  Created by COFFEE on 2015/09/04.
+//  Copyright (c) 2015年 COFFEE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WIKCamViewController : NSObject
+
+@end
---
+++ WIKCamViewController/WIKCamViewController.m
@@ -0,0 +1,13 @@
+//
+//  WIKCamViewController.m
+//  WIKCamViewController
+//
+//  Created by COFFEE on 2015/09/04.
+//  Copyright (c) 2015年 COFFEE. All rights reserved.
+//
+
+#import "WIKCamViewController.h"
+
+@implementation WIKCamViewController
+
+@end
