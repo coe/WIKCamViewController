34c98d3e4f92b79058f93ce3cd070e1438659cd5
coe <coe.app@gmail.com>
2015-11-13 15:54:43 +0900

    New Header Commit

---
+++ WIKCamViewController/WIKFacialRecognition.h
@@ -0,0 +1,16 @@
+//
+//  WIKFacialRecognition.h
+//  WIKCamViewController
+//
+//  Created by COFFEE on 2015/11/13.
+//  Copyright © 2015年 COFFEE. All rights reserved.
+//
+
+#ifndef WIKFacialRecognition_h
+#define WIKFacialRecognition_h
+
+#import "SquareCamViewController.h"
+#import "WIKCamViewController.h"
+#import "WIKFacialRecognitionController.h"
+
+#endif /* WIKFacialRecognition_h */
